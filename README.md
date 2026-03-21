34eafa19aea9399efd5950153a9b001eae516a70
LethaboMahlakwane23 <lethabochris0505@gmail.com>
2026-03-21 19:03:08 +0200

    Initial project structure: Data, Scripts, and Output
    Data: Eskom demand and weather CSVs.
    
    Scripts: Seasonal and weather-stressed demand analysis (R).
    
    Output: Visualizations for Problem1 & 2.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # SA-Energy-Demand-Analysis 2021-2026
-The project analyzes electricity demand patterns, focusing on how extreme temperatures affect consumption and variability, using statistical methods to quantify differences between normal, hot, and cold days.
+
+**Project Overview:** An investigation into South Africa's electricity demand patterns, focusing on seasonal variability and grid stress during extreme weather events.
+
+
+
+**Problem 1: Hourly Electricity Demand Patterns**
+
+* Hourly demand was aggregated by weekday vs weekend and by season.
+* Weekdays show sharper evening peaks (\~17:00–20:00) than weekends.
+* Seasonal variation: peaks are higher in winter and slightly flatter in summer.
+* Takeaway: Demand patterns vary by both day type and season, reflecting human activity and heating/cooling needs.
+
+
+
+**Problem 2:Peak Hour Demand on Extreme-Temperature Days**
+
+* Daily average temperature was calculated across Durban, Cape Town, and Johannesburg.
+* Days were classified as Very Hot (top 5%), Very Cold (bottom 5%), or Normal.
+* Mean demand during 17:00–20:00: Very Hot = 24,799 MW (−6.3%), Very Cold = 29,219 MW (+10.3%), Normal = 26,469 MW.
+* Takeaway: Peak demand rises on very cold days and slightly drops on very hot days.
+
+
+
+**Tech Stack**
+
+***Language***: R
+
+***Libraries***: tidyverse, ggplot2, lubridate, dplyr
+
---
+++ Scripts/Seasonal Demand and Extreme Weather impacts.R
@@ -0,0 +1,172 @@
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+library(dplyr)
+
+#########################################Problem 1###############################
+df <- read.csv("Eskom demand Data.csv")
+
+head(df[[1]])
+
+df <- df %>%
+  mutate(
+    datetime = parse_date_time(Date.Time.Hour.Beginning,
+                               orders = c("dmy HMS p", "ymd HMS p",
+                                          "dmy HM p",  "ymd HM p"))
+  )                                                                 ##Why? so we can group the dates into weekdays and weekends, compare trends time patterns.
+
+##Extracting the usable features
+df <- df %>%
+  mutate(
+    hour = hour(datetime),
+    date = as.Date(datetime),
+    weekday = wday(datetime, label = TRUE),
+    is_weekend = weekday %in% c("Sat", "Sun") # True is weekend
+  )
+
+# 2. Aggregate to get average demand per hour for weekdays and weekends
+hourly_curve <- df %>%
+  group_by(hour, is_weekend) %>%
+  summarise(avg_demand = mean(Residual.Demand, na.rm = TRUE)) %>%
+  mutate(day_type = ifelse(is_weekend, "Weekend", "Weekday"))
+
+
+# 3. Plot the average daily curve
+ggplot(hourly_curve, aes(x = hour, y = avg_demand, color = day_type)) +
+  geom_line(size = 1.2) +
+  labs(
+    title = "Hourly Electricity Demand: Weekday vs Weekend",
+    x = "Hour of Day",
+    y = "Average Demand (MW)",
+    color = "Day Type"
+  ) +
+  theme_minimal()
+
+# 4. Season column
+df<- df %>%
+  mutate(
+    month = month(date),
+    season = case_when(
+      month %in% c(12, 1, 2) ~ "Summer",
+      month %in% c(3, 4, 5)  ~ "Autumn",
+      month %in% c(6, 7, 8)  ~ "Winter",
+      month %in% c(9, 10, 11) ~ "Spring"
+    )
+  )
+
+# 5. Aggregate hourly demand by season
+seasonal_curve <- df %>%
+  group_by(hour, season) %>%
+  summarise(avg_demand = mean(Residual.Demand, na.rm = TRUE))
+
+ggplot(seasonal_curve, aes(x = hour, y = avg_demand, color = season)) +
+  geom_line(size = 1.2) +
+  labs(
+    title = "Hourly Electricity Demand by Season",
+    x = "Hour of Day",
+    y = "Average Demand (MW)"
+  ) +
+  theme_minimal()
+
+######################################Problem 2###########################################
+library(dplyr)
+#1. transfer data
+temp1 <- read.csv("Durban Weather 2021-2025.csv")
+temp2 <- read.csv("Cape Town Weather 2021-2025.csv")
+temp3 <- read.csv("Johannesburg Weather 2021-2025.csv")
+
+#2. Convert data to numeric
+temp1 <- temp1 %>%
+  mutate(
+    datetime = ymd_hm(time),
+    temperature_2m = as.numeric(temperature_2m),
+    rain._mm = as.numeric(rain._mm)
+  )
+
+temp2 <- temp2 %>%
+  mutate(
+    datetime = ymd_hm(time),
+    temperature_2m = as.numeric(temperature_2m),
+    rain_mm = as.numeric(rain_mm)
+  )
+
+temp3 <- temp3 %>%
+  mutate(
+    datetime = ymd_hm(time),
+    temperature_2m = as.numeric(temperature_2m),
+    rain_mm = as.numeric(rain_mm)
+  )
+
+#3. variable time has to be separated into a date column
+temp1 <- temp1 %>%
+  mutate(date = as.Date(datetime))
+
+temp2 <- temp2 %>%
+  mutate(date = as.Date(datetime))
+
+temp3 <- temp3 %>%
+  mutate(date = as.Date(datetime))
+
+#4. combining both cities
+temp_all <- bind_rows(temp1, temp2, temp3)
+
+#5. daily average temp
+daily_temp <- temp_all %>%
+  group_by(date) %>%
+  summarise(
+    avg_temp = mean(temperature_2m, na.rm = TRUE)
+  )
+head(daily_temp)
+summary(daily_temp$avg_temp)
+
+# Top 5% = hot days, Bottom 5% = cold days
+hot_threshold <- quantile(daily_temp$avg_temp, 0.95, na.rm = TRUE) #95th percentile
+cold_threshold <- quantile(daily_temp$avg_temp, 0.05, na.rm = TRUE) #5th percentile
+
+#classifying extreme temp days
+daily_temp <- daily_temp %>%
+  mutate(
+    extreme = case_when(
+      avg_temp >= hot_threshold ~ "Very Hot",
+      avg_temp <= cold_threshold ~ "Very Cold",
+      TRUE ~ "Normal"
+    )
+  )
+
+df_peak <- df %>%
+  left_join(daily_temp, by = "date")
+
+#Step 3: Filter peak hours (17:00–20:00)
+df_peak_hours <- df_peak %>%
+  filter(hour >= 17 & hour <= 20, !is.na(extreme))
+
+#manually calculating normal temperature mean
+normal_mean <- df_peak_hours %>%
+  filter(extreme == "Normal") %>%
+  summarise(mean_demand = mean(Residual.Demand, na.rm = TRUE)) %>%
+  pull(mean_demand)
+
+#Calculate mean demand by temperature category
+peak_summary <- df_peak_hours %>%
+  group_by(extreme) %>%
+  summarise(
+    mean_demand_MW = mean(Residual.Demand, na.rm = TRUE)
+  ) %>%
+  mutate(
+    perc_diff_vs_normal = 100 * (mean_demand_MW - normal_mean) / normal_mean
+  )
+
+peak_summary
+
+#Problem 2 Plot
+ggplot(peak_summary, aes(x = extreme, y = mean_demand_MW, fill = extreme)) +
+  geom_col(width = 0.6) +
+  geom_text(aes(label = paste0(round(perc_diff_vs_normal, 1), "%")), 
+            vjust = -0.5, size = 4) +
+  labs(
+    title = "Average Peak Hour Electricity Demand by Temperature Category",
+    x = "Temperature Category",
+    y = "Mean Demand (MW)"
+  ) +
+  scale_fill_manual(values = c("Very Hot" = "red", "Very Cold" = "blue", "Normal" = "grey")) +
+  theme_minimal()
